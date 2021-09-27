4bb79cf8402db496a4857d238a178ea97734a2a9
Fengleaf <as61002@gmail.com>
2021-09-28 02:52:15 +0800

    End member list.

---
+++ Assets/Scripts/Object/EndMemberList.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndMemberList : MonoBehaviour
+{
+    public float speed;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Run());
+    }
+
+    public IEnumerator Run()
+    {
+        while (transform.position.y < maxY)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Object/GameMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void Open()
+    {
+        menu.SetActive(true);
+    }
+
+    public void Close()
+    {
+        menu.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
