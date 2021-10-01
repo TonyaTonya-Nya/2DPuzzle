6ce70920929f04b5f20f4fe23aa84d5bee9c12d3
吳俊漳 <a0979790220@gmail.com>
2021-10-02 00:46:27 +0800

    BugFix 8 11 13 17 18 20

---
+++ Assets/Scripts/Object/EndAnime.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndAnime : MonoBehaviour
+{
+    public GameObject bg;
+    public SpriteRenderer title;
+    public GameObject gameList;
+    public GameObject againBtn;
+    public GameObject exitBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        title.color = new Color(title.color.r, title.color.g, title.color.b, title.color.a - 0.1f/60f);
+
+        if (title.color.a <= 0.05f)
+        {
+            gameList.SetActive(true);
+        }
+
+        if (gameList.transform.position.y > 650)
+        {
+            bg.SetActive(true);
+            againBtn.SetActive(true);
+            exitBtn.SetActive(true);
+        }
+    }
+}
